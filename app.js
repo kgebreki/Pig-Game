5ad1db142ed78eee4c1f8d23eabbbde164cb1d38
ksgebrekirstos98 <ksgebrekirstos98@gmail.com>
2020-01-13 16:30:16 -0500

    added init function

--- app.js
+++ app.js
@@ -10,16 +10,7 @@ GAME RULES:
 */
 
 let scores, roundScore, activePlayer;
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-// Initialization
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
+Init();
 
 // Event listener and handler for rolling dice
 document.querySelector('.btn-roll').addEventListener('click', function () {
@@ -53,6 +44,27 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         NextPlayer();
     }
 });
+
+// Event listener and handler for new game
+document.querySelector('.btn-new').addEventListener('click', Init);
+
+// Initialize
+function Init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
 
 // Next player
 function NextPlayer() {
